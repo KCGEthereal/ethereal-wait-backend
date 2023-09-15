f6da66ee67d926e6233df486f87e776133ceefb1
Karan Sanjeev <yo@alphaman.me>
2023-09-16 00:12:28 +0530

    Fix docker compose

---
+++ database/gorm.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"fmt"
+	"github.com/esportsclub/entity-service-golang/database/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
+
+// NewGorm initiates, checks and returns a gorm connection
+func NewGorm() (client *gorm.DB) {
+	user := os.Getenv("DATABASE_USER")
+	pass := os.Getenv("DATABASE_PASS")
+	name := os.Getenv("DATABASE_NAME")
+	host := os.Getenv("DATABASE_HOST")
+	port := os.Getenv("DATABASE_PORT")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, pass, host, port, name)
+	client, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Unable to connect to database")
+	}
+
+	if err = client.AutoMigrate(&models.Form{}); err != nil {
+		log.Fatal("Unable to migrate tables")
+	}
+
+	return
+}
